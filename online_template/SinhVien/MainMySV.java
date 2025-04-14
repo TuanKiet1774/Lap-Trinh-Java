9730ef86f508f256dd013dd5c3e47382f85be821
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-14 12:36:06 +0700

    Update MainMySV.java

--- online_template/SinhVien/MainMySV.java
+++ online_template/SinhVien/MainMySV.java
@@ -10,5 +10,20 @@ public class MainMySV {
 
         System.out.println("DÁNH SÁCH SINH VIÊN");
         dssv.hienThiDS();
+
+        System.out.println("\nTÌM THÔNG TIN SINH VIÊN");
+        int id = 64131616;
+        int id1 = 64131060;
+        var temp = dssv.findMyID(id1);
+        if(temp != null){
+            System.out.println(temp);
+        }
+        else
+            System.out.println("Không có thông tin của sinh viên có mã số " + id1);
+
+        System.out.println("\nCẬP NHẬT THÔNG TIN SINH VIÊN");
+        MySinhVien sv3 = new MySinhVien(64131600,"Diệp Ngọc Song","17/06/2004","Khánh Hòa");
+        dssv.update(sv3);
+        dssv.hienThiDS();
     }
 }
